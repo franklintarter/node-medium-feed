c18dca237d3e74e8dd8fd11f5d22ffb7630cea95
Franklin Tarter <franklintarter@gmail.com>
2018-12-04 21:36:07 -0800

    add toMarkdown on content

--- src/Story.ts
+++ src/Story.ts
@@ -5,25 +5,20 @@ export default class Story {
   /**
    * @param  {LooseObject} json
    */
-  constructor(
-    json : LooseObject
-  ) {
+  constructor(json: LooseObject) {
     this.updatedAt = new Date(json.updatedAt)
     this.createdAt = new Date(json.createdAt)
-    this.content = new Content(
-      json.content.subtitle,
-      json.content.bodyModel.paragraphs
-    )
+    this.content = new Content(json.content.subtitle, json.content.bodyModel.paragraphs)
     this.id = json.id
     this.title = json.title
     this.detectedLanguage = json.detectedLanguage
   }
-  public id : String
-  public title : String
-  public detectedLanguage : String
-  public updatedAt : Date
-  public createdAt : Date
-  public content : Content
+  public id: String
+  public title: String
+  public detectedLanguage: String
+  public updatedAt: Date
+  public createdAt: Date
+  public content: Content
 }
 
 export class Content {
@@ -31,12 +26,11 @@ export class Content {
    * @param  {String} publicsubtitle
    * @param  {LooseObject[]} paragraphs
    */
-  constructor(
-    public subtitle : String,
-    paragraphs: LooseObject[]
-  ) {
+  constructor(public subtitle: String, paragraphs: LooseObject[]) {
     this.paragraphs = paragraphs.map(p => new Paragraph(p))
   }
-  
-  public paragraphs : Paragraph[]
+  public toMarkdown(): string {
+    return this.paragraphs.reduce((p, v) => (p += v.processParagraph()), '')
+  }
+  public paragraphs: Paragraph[]
 }
